7cf6778747bbcd142f6bc27dcce608c02ac76435
gretzkiy <dias7707@gmail.com>
2019-12-14 01:31:58 +0300

    Веб-сервис для получения и отображения данных

---
+++ EmissionsWeb/EmissionsWebService.asmx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using EmissionsLibrary;
+
+namespace EmissionsWeb
+{
+    /// <summary>
+    /// Сводное описание для EmissionsWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку. 
+    // [System.Web.Script.Services.ScriptService]
+    public class EmissionsWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Привет всем!";
+        }
+
+        [WebMethod]
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        [WebMethod]
+        public string TestMethod(TransferData[] transferData)
+        {
+            try
+            {
+                return transferData[0].value;
+            }
+            catch
+            {
+                return "error in testmethod";
+            }
+        }
+    }
+}
